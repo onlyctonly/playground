46baf7577da0194613679f2bb95872b267008738
XiangYu Jia <jxyu@HaiJuandeiMac.local>
2017-07-01 18:14:39 +0300

    go

---
+++ rename/cp.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+func main() {
+	s:=6189546850
+	for i:=0;i<100;i++{
+		os.Link("/Users/jxyu/Downloads/批量上传/pic/base.jpg", "/Users/jxyu/Downloads/批量上传/pic/"+strconv.Itoa(s)+".jpg")
+		s++
+	}
+}
