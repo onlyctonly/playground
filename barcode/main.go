df274d895c09c9771c060d5f13fcb5904d9b60d6
xiangyu <onlyctonly@gmail.com>
2017-07-08 12:04:16 +0300

    go

---
+++ barcode/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"io"
+)
+
+func main() {
+	const head string = "0;"
+	const tail string = ";2;2;4;0;000000;1\n"
+
+	f,err:= os.Open("tk.txt")
+	checkerr(err)
+
+	f2,_:=os.Create("import.csv")
+	checkerr(err)
+
+	scanner:=bufio.NewScanner(f)
+	for scanner.Scan() {
+		io.WriteString(f2, head + scanner.Text() + tail)
+	}
+
+
+}
+
+func checkerr(e error)  {
+	if e!=nil {
+		fmt.Println(e)
+	}
+}
